d620b44ae51498b38a2aebfd06c42aa8863c2157
Pushpendra160 <pushpendrathakur145@gmail.com>
2024-05-31 11:15:00 +0530

    Initial commit

---
+++ movix-main/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+
+import { ClerkProvider } from '@clerk/clerk-react'
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+    </Provider>
+);
---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDb = require('./config/connectDb');
+
+const app = express();
+
+
+//database call
+connectDb();
+
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// config dot env file
+dotenv.config();
+
+//routes
+// user routes
+app.use('/api/v1/users', require('./routes/userRoute'));
+
+
+//transection routes
+
+
+
+
+
+const PORT = 8080  || process.env.PORT
+
+//listerning
+
+
+app.listen(PORT, (req, res)=>{
+    console.log(`Server is listening on ${PORT}`)
+});
